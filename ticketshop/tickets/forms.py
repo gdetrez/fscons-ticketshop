4d50deae2619bd42c6b302612b514963f51a9533
Grégoire Détrez <gregoire.detrez@gu.se>
2013-09-22 17:16:57 +0200

    Improve form

--- ticketshop/tickets/forms.py
+++ ticketshop/tickets/forms.py
@@ -1,10 +1,25 @@
 from django.forms import ModelForm
+from django.forms import TextInput, Select
 from .models import TicketPurchase, Ticket
 from django.forms.models import inlineformset_factory
 
 class TicketPurchaseForm(ModelForm):
     class Meta:
         model = TicketPurchase
+        widgets = {
+          'name': TextInput(attrs={'class':'form-control'}),
+          'email': TextInput(attrs={'class':'form-control'}),
+          }
 
-TicketFormSet = inlineformset_factory(TicketPurchase, Ticket)
 
+class TicketForm(ModelForm):
+    class Meta:
+        model   = Ticket
+        widgets = {
+            'name':         TextInput(attrs={'class':'form-control', 'placeholder': 'Name on badge'}),
+            'ticket_type':  Select(attrs={'class':'form-control'}),
+        }
+
+TicketFormSet = inlineformset_factory(TicketPurchase, Ticket,
+    extra = 1,
+    form=TicketForm)
