1580b8dcc4413b43ff6263d3ac4581196b887f27
Grégoire Détrez <gregoire.detrez@gu.se>
2013-10-14 17:12:47 +0200

    Add custom action to re-send confirmation emails

--- ticketshop/ticketapp/admin.py
+++ ticketshop/ticketapp/admin.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
+from django.contrib import messages
+
 from .models import TicketPurchase, Ticket, TicketType, Coupon
+from .mails import send_confirmation_email
 
 # ~~~ Ticket purchase ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 class TicketInline(admin.TabularInline):
@@ -21,6 +24,18 @@ def mark_selected_paid(modeladmin, request, queryset):
 mark_selected_paid.short_description = "Mark selected as paid"
 
 
+def resend_confirmation(modeladmin, request, queryset):
+    """
+    Custom admin action that re-send a confirmation email for the selected
+    purchases. Note that the purchases need to be marked as paid otherwise
+    the action will silently skip them
+    """
+    for purchase in queryset:
+        if purchase.paid:
+            send_confirmation_email(purchase)
+        else:
+            messages.warning(request, "Skiped unpaid purchase: %s. Use \"Mark selected as paid instead\"" % purchase)
+resend_confirmation.short_description = "Re-send confirmation email"
 
 class TicketPurchaseAdmin(admin.ModelAdmin):
     list_display = ('name', 'email', 'number_of_tickets', 'price', 'invoice_id', 'paid')
@@ -28,7 +43,7 @@ class TicketPurchaseAdmin(admin.ModelAdmin):
     inlines = [TicketInline]
     list_filter = ['paid','coupon', 'ticket__ticket_type']
     search_fields = ['name', 'email', 'additional_information', 'invoice_id']
-    actions = [mark_selected_paid]
+    actions = [mark_selected_paid, resend_confirmation]
 
 
 admin.site.register(TicketPurchase, TicketPurchaseAdmin)
