d263b36792303e0976911fedb590b0d7e32cde94
Grégoire Détrez <gregoire.detrez@gu.se>
2013-10-14 16:27:05 +0200

    Add action to the admin pannel to mark purchases as paid

--- ticketshop/ticketapp/admin.py
+++ ticketshop/ticketapp/admin.py
@@ -5,6 +5,12 @@ class TicketInline(admin.TabularInline):
     model = Ticket
     extra = 1
 
+def mark_selected_paid(modeladmin, request, queryset):
+    for purchase in queryset:
+        purchase.mark_as_paid()
+mark_selected_paid.short_description = "Mark selected stories as paid"
+
+
 class TicketPurchaseAdmin(admin.ModelAdmin):
     list_display = ('name', 'email', 'number_of_tickets', 'price', 'invoice_id', 'paid')
     date_hierarchy = 'creation_date'
@@ -12,6 +18,8 @@ class TicketPurchaseAdmin(admin.ModelAdmin):
     list_editable = ('paid',)
     list_filter = ['paid','coupon', 'ticket__ticket_type']
     search_fields = ['name', 'email', 'additional_information', 'invoice_id']
+    actions = [mark_selected_paid]
+
 
 admin.site.register(TicketPurchase, TicketPurchaseAdmin)
 admin.site.register(TicketType)
